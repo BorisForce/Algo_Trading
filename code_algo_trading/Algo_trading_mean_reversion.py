d757b32a8968f277e5257e6f99db5f5eec140063
BorisGerat <geratb3@gmail.com>
2025-06-18 21:53:47 +0200

    Create Algo_trading_mean_reversion.py

---
+++ code_algo_trading/Algo_trading_mean_reversion.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt 
+
+tickers = ['KO', 'PEP'] 
+start_date = '2020-01-01' 
+end_date = '2024-12-31' 
+data = yf.download(tickers, start= start_date, end=end_date)['Adj Close'] 
+print(data) 
